fb74abf4099f3fffd77e571b32eaa5e55699807a
pakonumb <al2troccoli@gmail.com>
2017-03-14 11:54:58 +0100

    adding waypoint js for the sticky menu, adding scroll animation with jquery

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
     
     
+    // STICKY menu settings
+   $(".js--section-features").waypoint(function(direction){
+       
+       if(direction === "down"){
+           $("nav").addClass("sticky");
+       }
+       else{
+           $("nav").removeClass("sticky");
+       }
+   },{
+       offset: '60px;'
+   }) 
+   
+   // Scroll animation
+   $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
     
 });
